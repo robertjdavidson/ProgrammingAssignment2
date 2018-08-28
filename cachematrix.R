dfa095a714a72e87554b38a7dce3cd6c6ae8e06e
Robert Davidson <robertjdavidson@gmail.com>
2018-08-28 21:31:05 +1000

    Adding implementation for required functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Based on makeVector example, it accepts a matrix and returns a "list"
+## of functions that help with caching of a single matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) i <<- inverse
+    getinverse <- function() i
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+    
 }
 
 
-## Write a short comment describing this function
+## Based on cachemean example, it accepts a cachedMatrix and returns either 
+## the cached, solved version or it solves the matrix and returns it.
+## Usage:
+## foo <- makeCacheMatrix(myMatrix)
+## bar <- cacheSolve(foo)
+## Running it again will returned the cached answer as confirmed by the call to message()
+## bar <- cacheSolve(foo)
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    i <- x$getinverse()
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinverse(i)
+    i
 }
